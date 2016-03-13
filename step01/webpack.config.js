2f2fe1e70203a38bc8540b37e35f4981676d9d1c
Shane Brinkman-Davis <shanebdavis@gmail.com>
2016-03-13 09:47:23 -0600

    simpler webpack.config.js

--- step01/webpack.config.js
+++ step01/webpack.config.js
@@ -1,7 +1,2 @@
 require('coffee-script/register');
-
-module.exports = require("art-foundation/configure_webpack")({
-  entries: "app",
-  dirname: __dirname
-});
-
+module.exports = require("art-foundation/configure_webpack")({entries: "app", dirname: __dirname});
